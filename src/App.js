d7ef65c107ead6d5da606e0f1b6b874818cc51da
EmiRoberti77 <emiliano.roberti@accenture.com>
2024-04-04 09:27:32 +0100

    added a mutation handler too

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { useEffect } from 'react';
 import config from './aws-exports';
 import { getBlog } from './graphql/queries';
+import { createBlog as createBlogMutation } from './graphql/mutations';
 import { generateClient } from 'aws-amplify/api';
 const client = new generateClient();
 function App() {
@@ -17,25 +18,25 @@ function App() {
 
       console.log(data);
     };
-
     pullData('12');
   }, []);
+
+  const createBlog = async () => {
+    const name = prompt('Blog name?');
+    const data = client.graphql({
+      query: createBlogMutation,
+      variables: {
+        input: {
+          name,
+        },
+      },
+    });
+
+    console.log('created blog', data);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={createBlog}>add blog</button>
     </div>
   );
 }
